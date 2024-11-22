c6e37031408400f64f2814c3256680fe5ca2e953
Kater1st <kat24007@byui.edu>
2024-11-22 23:05:39 +0200

    week01

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+  
+    
+    const lastModifiedDate = document.lastModified;
+    document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
+  });
+  
