82b0971bead19040280bde5871ed726ab4df7ac0
Kater1st <kat24007@byui.edu>
2024-11-24 01:59:32 +0200

    defer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,8 @@
-  window.addEventListener("DOMContentLoaded", () => {
-  
+document.addEventListener("DOMContentLoaded", function () {
+
   const currentYear = new Date().getFullYear();
   document.getElementById("currentyear").textContent = currentYear;
 
-  
   const lastModified = document.lastModified;
-  document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+  document.getElementById("lastModified").textContent = `Last modified: ${lastModified}`;
 });
